00a58757dc6a0ab1c86dd83b508cc84f8c573c72
francescopozzi <peppo.pozzi@gmail.com>
2016-06-09 16:06:34 +0200

    tolti file doppi

--- dbCredentials.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// php per le credenziali di accesso al db
-define("DB_ADDR", "localhost");
-define("DB_USER", "root");
-define("DB_PASS", "root");
-define("DB_NAME", "HypermediaDB");
-
-?>
