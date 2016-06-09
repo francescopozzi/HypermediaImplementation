49a9bc025c67986490a2c25bb4d9cad3116da74a
francescopozzi <peppo.pozzi@gmail.com>
2016-06-09 16:03:05 +0200

    unione lavoro richard e peppo, ridisposizione file e cartelle

---
+++ php/dbCredentials.php
@@ -0,0 +1,9 @@
+<?php
+
+// php per le credenziali di accesso al db
+define("DB_ADDR", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "root");
+define("DB_NAME", "HypermediaDB");
+
+?>
