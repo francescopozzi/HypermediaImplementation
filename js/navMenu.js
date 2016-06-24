c3c88e49c8b29e21d8c307d1411b33f49b3fbe16
vitaliRichard <rich.vita93@gmail.com>
2016-06-24 14:56:45 +0200

    sistemato light slider ecc

--- js/navMenu.js
+++ js/navMenu.js
@@ -3,28 +3,35 @@ function ready(){
     $("#1").on("click", attivaSpecifiche);
     $("#2").on("click", attivaSmartlife);
     $("#3").on("click", attivaAssistenza);
-    $("#specifiche").show();
-    $("#toSmarlife").hide();
+    $("#light-slider").show();
+    $("#specifiche").hide();
     $("#toAssistenza").hide();
+    
     
 }
 
 function attivaSpecifiche(){
     $("#SL").removeClass("azzurro");
     $("#AS").removeClass("azzurro");
+    $("#SL").addClass("sfondoGrigioChiaro");
+    $("#AS").addClass("sfondoGrigioChiaro");
     $("#SP").removeClass("sfondoGrigioChiaro");
     $("#SP").addClass("azzurro");
     $("#specifiche").show();
-    $("#toSmarlife").hide();
+    $("#light-slider").hide();
     $("#toAssistenza").hide();
+    $("li").hide();
 }
 
 function attivaSmartlife(){
     $("#SP").removeClass("azzurro");
     $("#AS").removeClass("azzurro");
+    $("#SP").addClass("sfondoGrigioChiaro");
+    $("#AS").addClass("sfondoGrigioChiaro");
     $("#SL").removeClass("sfondoGrigioChiaro");
     $("#SL").addClass("azzurro");
-    $("#toSmarlife").show();
+    $("li").show();
+    $("#light-slider").show();
     $("#specifiche").hide();
     $("#toAssistenza").hide();
 }
@@ -32,10 +39,13 @@ function attivaSmartlife(){
 function attivaAssistenza(){
     $("#SP").removeClass("azzurro");
     $("#SL").removeClass("azzurro");
+    $("#SP").addClass("sfondoGrigioChiaro");
+    $("#SL").addClass("sfondoGrigioChiaro");
     $("#AS").removeClass("sfondoGrigioChiaro");
     $("#AS").addClass("azzurro");
     $("#toAssistenza").show();
-    $("#toSmarlife").hide();
+    $("#light-slider").hide();
     $("#specifiche").hide();
+    $("li").hide();
 }
 
--- js/paginaDispositivo.js
+++ js/paginaDispositivo.js
@@ -1,10 +1,30 @@
 $(document).ready(ready);
 function ready(){
     
-    var idDispositivo = getQueryVariable("id")
+    var idDispositivo = getQueryVariable("id");
     
     getFromDB(idDispositivo);
+    
+    getSmartLife(idDispositivo);
+    
 
+}
+
+/**
+* Per Slider
+**/
+var immagini = 0;
+function aggiorna() {
+    immagini--;
+    if (immagini == 0) {
+        var slider = $('#light-slider').lightSlider({
+            item:4,
+            currentiPagerPosition:'middle',
+            onSliderLoad: function() {
+                $('#light-slider').removeClass('cS-hidden');
+            }  
+        });
+    }
 }
 
 function getFromDB(identificativo){
@@ -27,6 +47,11 @@ function getFromDB(identificativo){
             var descrizioneDispositivo = "";
             
             var specificheDispositivo = "";
+            
+            var bottone = "";
+            
+            bottone +=  "<a href='./"+ getLinkCategoria(dispositivi[0].Gruppo) +"' class='blu'>"+ getNomeCategoria(dispositivi[0].Gruppo) + 
+                "</a>";
 
             immagineDispositivo+="<img src='"+dispositivi[0].Immagine+"'"
             +"alt='telefono' class='imgDispositiviGrande'>";
@@ -35,6 +60,7 @@ function getFromDB(identificativo){
             descrizioneDispositivo+=dispositivi[0].Descrizione;
             specificheDispositivo+=dispositivi[0].Specifiche;
             
+            $("#bottone").html(bottone);
             $("#img").html(immagineDispositivo);
             $("#nome").html(nomeDispositivo);
             $("#descrizione").html(descrizioneDispositivo);
@@ -50,6 +76,41 @@ function getFromDB(identificativo){
 
 }
 
+function getSmartLife(identificativo){
+  $.ajax({
+        method: "POST",
+        //dataType: "json", //type of data
+        crossDomain: true, //localhost purposes
+        data: {dispositivi:identificativo}, 
+        url: "http://progettohyp.altervista.org/php/getSmartLifePerDispositivi.php", //Relative or absolute path to file.php file
+        success: function(response) {
+
+            console.log(JSON.parse(response));
+
+            
+            var smartLife = JSON.parse(response);
+                        
+            var slidebar = "";
+            immagini = smartLife.length;
+            for(var i=0; i<smartLife.length; i++){
+              slidebar += "<li class='centro'>"
+                        + "<img src='"+ smartLife[i].Immagine + "' onload=\"aggiorna();\"class='dimImgSL'>"
+                        + "<h1 class='blu'>" + smartLife[i].Nome + "</h1>"
+                        + "</li>";
+            }
+            
+            
+            $("#light-slider").html(slidebar);
+            
+            
+        },
+        error: function(request,error) 
+        {
+            console.log("Error");
+        }
+    });       
+}
+
 
 
 // funzione trovata online per ottenere le variabili di una query string
@@ -62,4 +123,47 @@ function getQueryVariable(variable)
                if(pair[0] == variable){return pair[1];}
        }
        return(false);
+}
+
+function getNomeCategoria(id)
+{
+  var nome = "";
+  if(id == 0){
+    nome += "Smartphone e Telefoni";
+  }
+  else if(id == 1){
+    nome += "Tablet e Computer";
+  }
+  else if (id == 2){
+    nome += "Modem e Networking";
+  }
+  else if (id == 3) {
+    nome += "TV e SmartLiving";
+  }
+  else if (id == 4) {
+    nome += "Promozioni";
+  }
+  return(nome);
+}
+
+
+function getLinkCategoria(id)
+{
+  var nome = "";
+  if(id == 0){
+    nome += "smartphone.html";
+  }
+  else if(id == 1){
+    nome += "tablet.html";
+  }
+  else if (id == 2){
+    nome += "modem.html";
+  }
+  else if (id == 3) {
+    nome += "tv.html";
+  }
+  else if (id == 4) {
+    nome += "promozioni.html";
+  }
+  return(nome);
 }
