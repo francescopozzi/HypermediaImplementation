d27d65f4dccc0041cfb9177c4ae0c0efa4e6f41c
francescopozzi <peppo.pozzi@gmail.com>
2016-06-29 17:16:27 +0200

    phonegap 4

--- js/getHighlightsPerAssistenza.js
+++ js/getHighlightsPerAssistenza.js
@@ -2,20 +2,18 @@ $(document).ready(ready);
 function ready(){
 
     // è una funzione che chiama una funzione, qua non serve a niente
-    alert("Inizio");
     getFromDB();
-     alert("0");
+
 }
 
 function getFromDB(){
-    alert("1");
     $.ajax({
         method: "POST",
         //dataType: "json", //type of data
         crossDomain: true, //localhost purposes
         url: "http://progettohyp.altervista.org/php/getHighlights.php", //Relative or absolute path to file.php file
         success: function(response) {
-             alert("2");
+
             console.log(JSON.parse(response));
 
             var assistenze = JSON.parse(response);
@@ -38,7 +36,6 @@ function getFromDB(){
         },
         error: function(request,error) 
         {
-             alert("3");
             console.log("Error");
         }
     });
--- js/script.js
+++
@@ -1,5 +0,0 @@
-$(document).bind("mobileinit", function () { 
-	alert("bind");
-$.support.cors = true; 
-$.mobile.allowCrossDomainPages = true;
-});
